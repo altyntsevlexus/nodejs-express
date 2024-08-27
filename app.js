c0d4f4e89e6724dc916c080740e40909d4578072
Altyntsev Oleksii <altalex491@gmail.com>
2024-08-27 16:57:56 +0300

    fix

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
 
 const dev_db_url =
-  "mongodb+srv://your_user_name:your_password@cluster0.cojoign.mongodb.net/local_library?retryWrites=true&w=majority";
+  "mongodb+srv://altalex491:<db_password>@cluster0.q5j7lv9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 const mongoDB = process.env.MONGODB_URI || dev_db_url;
 
 main().catch((err) => console.log(err));
